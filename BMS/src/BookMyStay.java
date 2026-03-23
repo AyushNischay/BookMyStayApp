7b1f7f9adbfcbaca33716cc459ecf1b589f5212b
AyushNischay <al2278@srmist.edu.in>
2026-03-23 10:45:59 +0530

    UC1 done

---
+++ BMS/src/BookMyStay.java
@@ -0,0 +1,6 @@
+public class BookMyStay {
+    public static void main (String[] args){
+        System.out.println("welcome to hotel booking management system");
+        System.out.println("Hotel booking system V.1.0");
+    }
+}
